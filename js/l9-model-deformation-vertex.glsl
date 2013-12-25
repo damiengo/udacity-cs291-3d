c26b0a2ef3190064763fc593014f8814ae4204ad
damiengo <dam.gou@gmail.com>
2013-12-25 20:33:26 +0100

    Answer

--- js/l9-model-deformation-vertex.glsl
+++ js/l9-model-deformation-vertex.glsl
@@ -7,9 +7,10 @@ void main() {
 	// Student: modify position. You'll need to copy it to a
 	// temporary vector and modify the temporary. 
 	// Don't forget to use this temporary in the rest of the shader.
-    
-	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+    vec3 positionTmp = position;
+    positionTmp.z = sqrt(uSphereRadius2 - (positionTmp.x * positionTmp.x) - (positionTmp.y * positionTmp.y)) - sqrt(uSphereRadius2);
+	gl_Position = projectionMatrix * modelViewMatrix * vec4( positionTmp, 1.0 );
 	vNormal = normalize( normalMatrix * normal );
-	vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+	vec4 mvPosition = modelViewMatrix * vec4( positionTmp, 1.0 );
 	vViewPosition = -mvPosition.xyz;
 }
